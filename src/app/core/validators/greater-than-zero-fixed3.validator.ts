0a19924aa2d57cd9849d8dafa73bf89a0fc85530
Rodrigo Gonzalez Dominguez <rodrigo.gonzalezdomi@plexus.tech>
2025-02-27 13:54:40 +0100

    validators v2

--- src/app/core/validators/greater-than-zero-fixed3.validator.ts
+++ src/app/core/validators/greater-than-zero-fixed3.validator.ts
@@ -1,16 +1,19 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 // Validador personalizado para verificar que el valor sea mayor que 0 y con máximo 3 decimales (ejemplo 0.123)
-export function greaterThanZeroValidatorFixed3(): ValidatorFn {
+export function greaterThanZeroValidatorFixed3(minValue: number = 0.001): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const value = control.value;
-    if (value <= 0) {
-      return { greaterThanZero: { value: control.value } };
+    if (value === null || value === undefined || value <= 0 || value < minValue) {
+      return { greaterThanZero: { value: control.value, minValue } };
+    }
+    if (value < minValue) {
+      return { maxDecimals: { value: control.value } };
     }
     const decimalPart = (value.toString().split('.')[1] || '').length;
     if (decimalPart > 3) {
       return { maxDecimals: { value: control.value } };
     }
-    return null;
+    return null; 
   };
 }
